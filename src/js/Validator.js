d6991ef1e2d58a46713b8a012ed46c12eb58a0b8
NUishumb <localhost>
2022-02-26 11:24:32 +0700

    validator fix

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,6 +1,10 @@
 export default class Validator {
     // eslint-disable-next-line class-methods-use-this
     validateUsername(name) {
-        return /^[a-zA-Z]+\D+\d{0,3}[^\-_\0-9]$/.test(name);
+        const pattern = /[A-Za-z]/;
+        if (pattern.test(name[0]) && pattern.test(name[name.length - 1])) {
+            return name.match(/\d{4,}/) === null;
+        }
+        return false;
     }
 }
--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,37 +1,17 @@
 import Validator from '../Validator';
 
-test('username test case 0', () => {
-    const received = new Validator().validateUsername('test');
-
-    expect(received).toEqual(true);
-});
-
-test('username test case 1', () => {
-    const received = new Validator().validateUsername('Abc123d');
-
-    expect(received).toEqual(true);
-});
-
-test('username test case 2', () => {
-    const received = new Validator().validateUsername('testertestingtests6n');
-
-    expect(received).toEqual(true);
-});
-
-test('username test case 3', () => {
-    const received = new Validator().validateUsername('_test');
-
-    expect(received).toEqual(false);
-});
-
-test('username test case 4', () => {
-    const received = new Validator().validateUsername('test5');
-
-    expect(received).toEqual(false);
-});
-
-test('username test case 5', () => {
-    const received = new Validator().validateUsername('test6578g');
-
-    expect(received).toEqual(false);
+describe.each([
+    { received: 'test6578g', expected: false },
+    { received: 'Us11er', expected: true },
+    { received: 'Us11e22r', expected: true },
+    { received: 'User', expected: true },
+    { received: 'test55', expected: false },
+    { received: '_test', expected: false },
+    { received: 'te55st555s', expected: true },
+    { received: 'U77s77e77r', expected: true },
+    { received: 'user-', expected: false },
+])('.add($#)', ({ received, expected }) => {
+    test(`returns ${expected}`, () => {
+        expect(new Validator().validateUsername(received)).toBe(expected);
+    });
 });
